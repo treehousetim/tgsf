3b4a0ab1a3d32a52d13a82d5de186d403d77b996
treehousetim <treehousetim@b9c5c07c-5b76-11de-8c10-5fe6993ea61f>
2009-06-29 07:46:01 +0000

    forgot to commit 3 new files

---
+++ trunk/tgsf_core/libraries/db/selectQueryJoin.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+This code is copyright 2009 by TMLA INC.  ALL RIGHTS RESERVED.
+Please view license.txt in /tgsf_core/legal/license.txt or
+http://tgWebSolutions.com/opensource/tgsf/license.txt
+for complete licensing information.
+*/
+//------------------------------------------------------------------------
+class selectQueryJoin
+{
+	public $type = '';
+	public $foreignTable = '';
+	public $clause = '';
+	public function __construct( $type, $table, $clause )
+	{
+		$this->type = $type;
+		$this->foreignTable = $table;
+		$this->clause = $clause;
+	}
+	
+	//------------------------------------------------------------------------
+	/**
+	* Generates the SQL for the join.
+	*/
+	function generate()
+	{
+		$out = '';
+		$out .= ' ' . $this->type . ' ';
+		$out .= $this->foreignTable;
+		$out .= ' ON ( ';
+		$out .= trim( $this->clause, ' )(' ) . ' ) ';
+		
+		return $out;
+	}
+}
