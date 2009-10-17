f56802658fbe9972b9276e7a91696528d6bd7aad
Tim Gallagher <treehousetim@users.noreply.github.com>
2009-10-17 15:22:29 +0000

    new default home page content

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,11 +6,21 @@ http://tgWebSolutions.com/opensource/tgsf/license.txt
 for complete licensing information.
 */
 
+// see the config/config.php for the site name that is referenced in views/header.php
+$windowTitle = 'Welcome';
+
 
 include view( 'header' );
 ?>
-<body>
-<h2>
+<body style="margin: 1em;">
+<h2 style="font-size: 2em;">Welcome to tgsf</h2>
+<p>Congratulations!  If you don't see any error messages then you have successfully installed tgsf.</p>
+<br>
+<p>We have worked hard to make tgsf a framework that is useful, robust and easy to use and understand.</p>
+<p>Our goal is for you to work with tgsf the same way as you're already working with PHP.</p>
+<p>You can visit our <a href="http://code.google.com/p/tgsf/">Google Code</a> project page</a> for more information</p>
+<br>
+<p>You should not put your content directly in controllers like we've done with our sample home page.  Please use views instead of putting HTML output directly in your controller.  You'll be happier, we'll be happier, and everyone else will be happier and will give you a hearty thump on the back and congratulate you.</p>
 
 <?php
 
