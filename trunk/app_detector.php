e7725cd492378969231e5582b6ef8a884f39aee8
treehousetim <treehousetim@b9c5c07c-5b76-11de-8c10-5fe6993ea61f>
2009-10-17 15:23:18 +0000

    new .htaccess that should work in all cases new app_detector.php for multi-application installs

---
+++ trunk/app_detector.php
@@ -0,0 +1,16 @@
+<?php defined( 'BASEPATH' ) or die( 'Restricted' );
+
+// this file is not overwritten in upgrade distributions.
+// application detection goes here
+// which means that the app folder can be set using
+// any means you need - i.e. url inspection, etc.
+// all functions in the url detection core lib are loaded at this point.
+
+// if you need to use a database for application detection
+// you'll need this:
+// load_database_libraries();
+
+// then you'll need to set up a connection
+// dbm()->useSetup( new dbSetup( 'user', 'password', 'db_name' ) );
+
+define( 'APP_FOLDER', 'application/' );
