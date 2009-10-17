fa8693ca342b0466c1e6796f2e5177695d0db883
treehousetim <treehousetim@b9c5c07c-5b76-11de-8c10-5fe6993ea61f>
2009-10-17 15:30:23 +0000

    new security questions

---
+++ trunk/tgsf_core/config/security_questions.php
@@ -0,0 +1,41 @@
+<?php defined( 'BASEPATH' ) or die( 'Restricted' );
+/*
+This code is copyright 2009 by TMLA INC.  ALL RIGHTS RESERVED.
+Please view license.txt in /tgsf_core/legal/license.txt or
+http://tgWebSolutions.com/opensource/tgsf/license.txt
+for complete licensing information.
+*/
+$config['security_questions'] = array( 
+"What was your childhood nickname?" => "What was your childhood nickname?",
+"In what city did you meet your spouse/significant other?" => "In what city did you meet your spouse/significant other?",
+"What is the name of your favorite childhood friend?" => "What is the name of your favorite childhood friend?",
+"What is your oldest sibling's birthday month and year?" => "What is your oldest sibling's birthday month and year?",
+"What is the middle name of your youngest child?" =>"What is the middle name of your youngest child?",
+"What is your oldest sibling's middle name?" => "What is your oldest sibling's middle name?",
+"What school did you attend for sixth grade?" => "What school did you attend for sixth grade?",
+"What was your childhood phone number including area code?" => "What was your childhood phone number including area code?",
+"What is your oldest cousin's first and last name?" => "What is your oldest cousin's first and last name?",
+"What was the name of your first stuffed animal?" => "What was the name of your first stuffed animal?",
+"In what city or town did your mother and father meet?" => "In what city or town did your mother and father meet?",
+"What is the first name of the boy or girl that you first kissed?" => "What is the first name of the boy or girl that you first kissed?",
+"What was the last name of your third grade teacher?" => "What was the last name of your third grade teacher?",
+"Where does your nearest sibling live?" =>"Where does your nearest sibling live?",
+"What is your youngest brother's birthday?" => "What is your youngest brother's birthday?",
+"What was the name of your elementary / primary school?" => "What was the name of your elementary / primary school?",
+"What is your maternal grandmother's maiden name?" => "What is your maternal grandmother's maiden name?",
+"In what city or town was your first job?" => "In what city or town was your first job?",
+"What was your favorite place to visit as a child?" => "What was your favorite place to visit as a child?",
+"What is your spouse's mother's maiden name?" => "What is your spouse's mother's maiden name?",
+"What is the name of your favorite childhood teacher?" => "What is the name of your favorite childhood teacher?",
+"How many bones have you broken?" => "How many bones have you broken?",
+"What was your dream job as a child?" => "What was your dream job as a child?",
+"In what year was your father born?" => "In what year was your father born?",
+"In what year was your mother born?" => "In what year was your mother born?",
+"What is your mother's maiden name?" => "What is your mother's maiden name?",
+"What was the color of your first car?" => "What was the color of your first car?",
+"What is your father's middle name?" => "What is your father's middle name?",
+"What is your mother's middle name?" => "What is your mother's middle name?",
+"What is your favorite pet's name?" => "What is your favorite pet's name?",
+"Name of your high school mascot?" => "Name of your high school mascot?",
+"What year did you graduate from High School?" => "What year did you graduate from High School?"
+);
