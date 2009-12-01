644e7c62d81f5373d6311dfc23c284c5ffbe3a8e
treehousetim <treehousetim@b9c5c07c-5b76-11de-8c10-5fe6993ea61f>
2009-12-01 18:41:46 +0000

    sample names in core config

---
+++ trunk/tgsf_core/config/sample_names.php
@@ -0,0 +1,44 @@
+<?php defined( 'BASEPATH' ) or die( 'Restricted' );
+/*
+This code is copyright 2009 by TMLA INC.  ALL RIGHTS RESERVED.
+Please view license.txt in /tgsf_core/legal/license.txt or
+http://tgWebSolutions.com/opensource/tgsf/license.txt
+for complete licensing information.
+*/
+$config['sample_first_names'] = array(
+'Jacob','Emma','Michael','Isabella','Ethan','Emily','Joshua','Madison','Daniel','Ava','Alexander','Olivia','Anthony','Sophia','William','Abigail','Christopher',
+'Elizabeth','Matthew','Chloe','Jayden','Samantha','Andrew','Addison','Joseph','Natalie','David','Mia','Noah','Alexis','Aiden','Alyssa','James','Hannah','Ryan',
+'Ashley','Logan','Ella','John','Sarah','Nathan','Grace','Elijah','Taylor','Christian','Brianna','Gabriel','Lily','Benjamin','Hailey','Jonathan','Anna','Tyler',
+'Victoria','Samuel','Kayla','Nicholas','Lillian','Gavin','Lauren','Dylan','Kaylee','Jackson','Allison','Brandon','Savannah','Caleb','Nevaeh','Mason','Gabriella',
+'Angel','Sofia','Isaac','Makayla','Evan','Avery','Jack','Riley','Kevin','Julia','Jose','Leah','Isaiah','Aubrey','Luke','Jasmine','Landon','Audrey','Justin',
+'Katherine','Lucas','Morgan','Zachary','Brooklyn','Jordan','Destiny','Robert','Sydney','Aaron','Alexa','Brayden','Kylie','Thomas','Brooke','Cameron','Kaitlyn',
+'Hunter','Evelyn','Austin','Layla','Adrian','Madeline','Connor','Kimberly','Owen','Zoe','Aidan','Jessica','Jason','Peyton','Julian','Alexandra','Wyatt','Claire',
+'Charles','Madelyn','Luis','Maria','Carter','Mackenzie','Juan','Arianna','Chase','Jocelyn','Diego','Amelia','Jeremiah','Angelina','Brody','Trinity','Xavier',
+'Andrea','Adam','Maya','Carlos','Valeria','Sebastian','Sophie','Liam','Rachel','Hayden','Vanessa','Nathaniel','Aaliyah','Henry','Mariah','Jesus','Gabrielle',
+'Ian','Katelyn','Tristan','Ariana','Bryan','Bailey','Sean','Camila','Cole','Jennifer','Alex','Melanie','Eric','Gianna','Brian','Charlotte','Jaden',
+'Paige','Carson','Autumn','Blake','Payton','Ayden','Faith','Cooper','Sara','Dominic','Isabelle','Brady','Caroline','Caden','Genesis','Josiah','Isabel',
+'Kyle','Mary','Colton','Zoey','Kaden','Gracie','Eli','Megan' );
+
+$config['sample_first_names_count'] = count( $config['sample_first_names'] );
+
+$config['sample_last_names'] = array(
+'SMITH','JOHNSON','WILLIAMS','JONES','BROWN','DAVIS','MILLER','WILSON','MOORE','TAYLOR','ANDERSON','THOMAS','JACKSON','WHITE','HARRIS','MARTIN',
+'THOMPSON','GARCIA','M','RTINEZ','ROBINSON','CLARK','RODRIGUEZ','LEWIS','LEE','WALKER','HALL','ALLEN','YOUNG','HERNANDEZ','KING','WRIGHT','LOPEZ',
+'HILL','SCOTT','GREEN','ADAMS','BAKER','GONZ','LEZ','NELSON','CARTER','MITCHELL','PEREZ','ROBERTS','TURNER','PHILLIPS','CAMPBELL','PARKER','EVANS','EDWARDS',
+'COLLINS','STEWART','SANCHEZ','MORRIS','ROGERS','REED','COOK','MORGAN','BELL','MURPHY','BAILEY','RIVERA','COOPER','RICHARDSON','COX','HOWARD','WARD',
+'TORRES','PETERSON','GRAY','RAMIREZ','JAMES','WATSON','BROOKS','KELLY','SA','DERS','PRICE','BENNETT','WOOD','BARNES','ROSS','HENDERSON','COLEMAN','JENKINS',
+'PERRY','POWELL','LONG','PATTERSON','HUGHES','FLORES','WASHINGTON','BUTLER','SIMMONS','FOSTER','GONZALES','BRYANT','ALEXANDER','RUSSELL','GRIFFIN','DIAZ',
+'HAYES','MYERS','FORD','HAMILTON','GRAHAM','SULLIVAN','WALLACE','WOODS','COLE','WEST','JORDAN','OWENS','REYNOLDS','FISHER','ELLIS','HARRISON','GIBSON',
+'MCDONALD','CRUZ','MARSHALL','ORTIZ','GOMEZ','MURRAY','FREEMAN','WELLS','WEBB','SIMPSON','STEVENS','TUCKER','PORTER','HUNTER','HICKS','CRAWFORD','HENRY','BOYD',
+'MASON','MORALES','KENNEDY','WARREN','DIXON','RAMOS','REYES','BURNS','GORDON','SHAW','HOLMES','RICE','ROBERTSON','HUNT','BLACK','DANIELS','PALMER','MILLS','NICHOLS',
+'GRANT','KNIGHT','FERGUSON','ROSE','STONE','HAWKINS','DUNN','PERKINS','HUDSON','SPENCER','GARDNER','STEPHENS','PAYNE','PIERCE','','ERRY','MATTHEWS','ARNOLD',
+'WAGNER','WILLIS','RAY','WATKINS','OLSON','CARROLL','DUNCAN','SNYDER','HART','CUNNINGHAM','BRADLEY','LANE','ANDREWS','RUIZ','HARPER','FOX','RILEY','ARMSTRONG',
+'CARPENTER','WEAVER','GREENE','LAWRENCE','ELLIOTT','CHAVEZ','SIMS','AUSTIN','PETERS','KELLEY','FRANKLIN','LAWSON','FIELDS','GUTIERREZ','RYAN','SCHMI','T','CARR',
+'VASQUEZ','CASTILLO','WHEELER','CHAPMAN','OLIVER','MONTGOMERY','RICHARDS','WILLIAMSON','JOHNSTON','BANKS','MEYER','BISHOP','MCCOY','HOWELL','ALVAREZ','MORRISON',
+'HANSEN','FERNANDEZ','GARZA','HARVEY','LITTLE','BURTON','STANLEY','NGUYEN','GEORGE','JACOBS','REID','KIM','FULLER','LYNCH','DEAN','GILBERT','GARRETT','ROMERO',
+'WELCH','LARSON','FRAZIER','BURKE','HANSON','DAY','MENDOZA','MORENO','BOWMAN','MEDINA','FOWLER','BREWER','HOFFMAN','CARLSON','SILVA','PEARSON','HOLLAND','DOUGLAS',
+'FLEMING','JENSEN','VA','GAS','BYRD','DAVIDSON','HOPKINS','MAY','TERRY','HERRERA','WADE','SOTO','WALTERS','CURTIS','NEAL','CALDWELL','LOWE','JENNINGS','BARNETT',
+'GRAVES','JIMENEZ','HORTON','S','ELTON','BARRETT','OBRIEN','CASTRO','SUTTON','GREGORY','MCKINNEY','LUCAS','MILES','CRAIG','RODRIQUEZ','CHAMBERS','HOLT','LAMBERT',
+'FLETCHER','WATTS','BATES','HALE','RH','DES','PENA','BECK','NEWMAN','HAYNES','MCDANIEL' );
+
+$config['sample_last_names_count'] = count( $config['sample_last_names'] );
