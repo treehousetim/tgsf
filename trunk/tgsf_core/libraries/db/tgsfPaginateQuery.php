dc4da81b224aac7fb5728801daf2f3b8151763da
treehousetim <treehousetim@b9c5c07c-5b76-11de-8c10-5fe6993ea61f>
2010-07-21 06:08:03 +0000

    complete logic for paginated queries

--- trunk/tgsf_core/libraries/db/tgsfPaginateQuery.php
+++ trunk/tgsf_core/libraries/db/tgsfPaginateQuery.php
@@ -9,23 +9,33 @@ for complete licensing information.
 //------------------------------------------------------------------------
 class tgsfPaginateQuery extends query
 {
-	protected $_ro_maxRecords = 1500; // due to the performance of mysql's limit clause we don't allow more than this total records ever
+	// due to the performance of mysql's limit clause we don't allow more than this total records ever
+	// this is allowed to be overridden via the ->maxRecords method
+	// the point of this is that limits with a high starting record are slow so we only ever allow a total number of *records*
+	protected $_ro_maxRecords = 1500;
 	protected $_ro_curPage; // defaults in constructor
 	protected $_ro_resultsPer; // defaults in constructor
-	protected $_ro_totalResults; // set in the exec function
+	protected $_ro_totalResults = null; // set in the exec function
 	protected $_ro_enforceLimit = true;
-	
+	protected $_ro_totalPages = 0;
+
 	protected $_selectList	= array();
-	
+
+	public static function &factory( $which = 'default' )
+	{
+		$c = __CLASS__;
+		$instance = new $c( $which );
+		return $instance;
+	}
+	//------------------------------------------------------------------------
 	public function __construct( $which = 'default' )
 	{
 		parent::__construct( $which );
-		if ( GET()->dataPresent )
-		{
-			$this->_ro_curPage = GET()->_( 'page', 1 );
-			$this->_ro_resultsPer = GET()->_( 'per', 10 );
-			$this->_ro_totalResults = GET()->_( 'latot', 0 );
-		}
+
+		$this->_ro_curPage = GET()->_( 'page', 1 );
+		$this->_ro_resultsPer = GET()->_( 'per', 10 );
+		$this->_ro_totalResults = GET()->_( 'latot', NULL );
+		$this->_ro_totalPages = (int)( $this->_ro_totalResults/$this->_ro_resultsPer ) + 1;
 	}
 	//------------------------------------------------------------------------
 	/**
@@ -46,6 +56,23 @@ class tgsfPaginateQuery extends query
 	}
 	//------------------------------------------------------------------------
 	/**
+	*
+	*/
+	public function &resultsPer( $value )
+	{
+		$this->_ro_resultsPer = $value;
+		return $this;
+	}
+	//------------------------------------------------------------------------
+	/**
+	* Enforces the max record limit for the page
+	*/
+	protected function _getCurPage( $increment = 0 )
+	{
+		return min( max( 1,$this->curPage + $increment ), $this->_ro_maxRecords/$this->_ro_resultsPer );
+	}
+	//------------------------------------------------------------------------
+	/**
 	* Pass in your url object to have pagination variables set on it for the next page.
 	* @param Object::tgsfUrl The url object
 	*/
@@ -53,10 +80,10 @@ class tgsfPaginateQuery extends query
 	{
 		if ( empty( $this->_ro_totalResults ) )
 		{
-			$this->getPagCount();
+			$this->setPageCount();
 		}
 		
-		$url->setVar( 'page', min( $this->_ro_maxRecords, $this->_ro_curPage + 1 ) );
+		$url->setVar( 'page', $this->_getCurPage( 1 ) );
 		$url->setVar( 'per', $this->_ro_resultsPer );
 		$url->setVar( 'latot', $this->_ro_totalResults );
 
@@ -71,10 +98,10 @@ class tgsfPaginateQuery extends query
 	{
 		if ( empty( $this->_ro_totalResults ) )
 		{
-			$this->getPagCount();
+			$this->setPageCount();
 		}
 		
-		$url->setVar( 'page', max( 1, $this->_ro_curPage -1 ) );
+		$url->setVar( 'page', $this->_getCurPage( -1 ) );
 		$url->setVar( 'per', $this->_ro_resultsPer );
 		$url->setVar( 'latot', $this->_ro_totalResults );
 
@@ -84,18 +111,20 @@ class tgsfPaginateQuery extends query
 	/**
 	*
 	*/
-	public function &getPagCount()
+	public function &setPageCount()
 	{
-		if ( ! empty( $this->_ro_totalResults ) )
+		if ( $this->_ro_totalResults !== null )
 		{
-			return $this->_ro_totalResults;
+			return $this;
 		}
 		
 		$_selectList = $this->_selectList;
+		$this->_selectList = array();
 		$this->count();
 		parent::exec();
-		
+
 		$this->_ro_totalResults = $this->fetchColumn(0);
+		$this->_ro_totalPages = (int)( $this->_ro_totalResults/$this->_ro_resultsPer ) + 1;
 
 		$this->_stmHandle = null;
 		$this->_executed = false;
@@ -111,12 +140,12 @@ class tgsfPaginateQuery extends query
 	{
 		if ( empty( $this->_ro_totalResults ) )
 		{
-			$this->getPagCount();
+			$this->setPageCount();
 		}
 
 		if ( $this->enforceLimit )
 		{
-			$this->limit( ( $this->_ro_curPage * $this->_ro_resultsPer ) - $this->_ro_resultsPer . ", {$this->_ro_resultsPer}" );
+			$this->limit( ( $this->_getcurPage() * $this->_ro_resultsPer ) - $this->_ro_resultsPer . ", {$this->_ro_resultsPer}" );
 		}
 
 		return parent::exec();
